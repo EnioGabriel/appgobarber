0f544f98095882463959519bcb63397a7ab70334
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-07-21 20:04:49 -0300

    criando estrutura da tela

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,8 +1,43 @@
-import React from 'react';
-import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
 
 const AppointmentCreated: React.FC = () => {
-  return <View />;
+  // reset impede que o usuário clique em voltar e acesse as telas anteriores
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      // Estado da rota (tela) que quero resetar o usuário
+      routes: [
+        {
+          name: 'Dashboard',
+        },
+      ],
+      // Posição para setar a rota (tela) indicada, como tem só uma, usa o zero
+      index: 0,
+    });
+  }, [reset]);
+
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>Hora do agendamento</Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
+  );
 };
 
 export default AppointmentCreated;
